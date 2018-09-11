16ea6de285094c16455c26012ad26a40a0543b9a
Beau Jepson <beau.jepson@jepsonconsulting.com>
2018-09-11 19:12:48 +0000

    automatically backed up by learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,14 @@
+def using_push(array,foo)
+  array.push foo
+  return array
+end
+
+def using_unshift(array,foo)
+  array.unshift foo
+  return array
+end
+
+def using_pop(array)
+  array.pop
+  return array
+end
